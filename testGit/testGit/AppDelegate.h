e99b8795236b4c69bfe8f257deac812cdb599c46
npu3paka <cheetahbg93@yahoo.com>
2015-01-29 10:26:47 +0200

    Test All

---
+++ testGit/testGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by 158ltd Developer on 1/29/15.
+//  Copyright (c) 2015 158ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGit/testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by 158ltd Developer on 1/29/15.
+//  Copyright (c) 2015 158ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
